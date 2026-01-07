e0ab3c3df618fdada0b9485199d164b0daf3e83b
Akhil Jose <akhiljose@gmail.com>
2026-01-07 13:39:34 +0530

    add material.icons.extended library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.material.icons.core)
+    implementation(libs.material.icons.extended)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
