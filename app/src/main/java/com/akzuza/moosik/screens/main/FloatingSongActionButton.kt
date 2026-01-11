14049d6fe9f25675eb7c6d4f986d6a7d4f9d0d9e
Akhil Jose <akhiljose@gmail.com>
2026-01-11 12:45:20 +0530

    implement custom interaction source logic handles toggling of play state handles long press events into third state handles release event accordinly to exit the third state

--- app/src/main/java/com/akzuza/moosik/screens/main/FloatingSongActionButton.kt
+++ app/src/main/java/com/akzuza/moosik/screens/main/FloatingSongActionButton.kt
@@ -3,7 +3,11 @@ package com.akzuza.moosik.screens.main
 import android.util.Log
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
@@ -12,52 +16,79 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
-
-private fun Modifier.SongClickActions(
-    onClick: () -> Unit,
-    onLongPress: () -> Unit,
-    onLeftSwipe: () -> Unit = {},
-    onRightSwipe: () -> Unit= {},
-): Modifier = this.combinedClickable(
-    enabled = true,
-    onClick = onClick,
-    onLongClick = onLongPress,
-)
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 
 @Composable
 fun FloatingSongActionButton(
     modifier: Modifier = Modifier,
     play: Boolean,
-    onClick: () -> Unit,
-    onLeftSwipe: () -> Unit,
-    onRightSwipe: () -> Unit
+    togglePlayState: () -> Unit,
+    onFastForward: () -> Unit,
+    onFastBackward: () -> Unit
 ) {
     val colors = IconButtonDefaults.filledIconButtonColors(
         containerColor = MaterialTheme.colorScheme.primaryContainer
     )
-
     val shape = ShapeDefaults.Medium
+    var openMenu by remember { mutableStateOf(false) }
+    val displayIcon = if (openMenu) Icons.Default.Close else
+        if (play) Icons.Default.Pause else Icons.Default.PlayArrow
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val scope = rememberCoroutineScope()
+
+    var longPressJob: Job? = null
+
+    // Launched effect to keep track of press events and trigger long press event
+    LaunchedEffect(interactionSource) {
+        interactionSource.interactions.collect { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> {
+                    if (!openMenu) {
+                        longPressJob = scope.async {
+                            delay(600)
+                            openMenu = true
+                        }
+                    }
+                }
+
+                is PressInteraction.Release -> {
+                    longPressJob?.let {
+                        if (it.isActive) {
+                            longPressJob!!.cancel()
+                        }
+                    }
+
+                    if (openMenu && longPressJob == null) openMenu = false
+                    longPressJob = null
+                }
+            }
+        }
+    }
 
     IconButton(
-        modifier = modifier
-            .SongClickActions(
-                onClick = {},
-                onLongPress = { Log.d("Song", "FloatingSongActionButton: Long Pressed")}
-            ),
+        modifier = modifier,
         colors = colors,
         shape = shape,
-        onClick = onClick
+        onClick = {
+            if (!openMenu) togglePlayState()
+        },
+        interactionSource = interactionSource
     ) {
-        val icon = if (!play) Icons.Default.PlayArrow else Icons.Default.Pause
         Icon(
-            icon,
+            displayIcon,
             contentDescription = null,
             modifier = Modifier.scale(1.1f)
         )
@@ -70,8 +101,8 @@ fun FloatingSongActionButtonPreview() {
     var play by remember { mutableStateOf(false) }
     FloatingSongActionButton(
         play = play,
-        onClick = { play = !play },
-        onLeftSwipe = {},
-        onRightSwipe = {},
+        togglePlayState = { play = !play },
+        onFastForward = {},
+        onFastBackward = {}
     )
 }
