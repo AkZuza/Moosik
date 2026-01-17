656d0abfbffb956a05a7f403dc46135fe3f48ccf
Akhil Jose <akhiljose@gmail.com>
2026-01-17 16:03:44 +0530

    add status property to Session add enum class SessionStatus

--- app/src/main/java/com/akzuza/moosik/entities/Session.kt
+++ app/src/main/java/com/akzuza/moosik/entities/Session.kt
@@ -3,7 +3,13 @@ package com.akzuza.moosik.entities
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
+enum class SessionStatus {
+    Empty, Play, Pause, Completed
+}
+
 data class Session @OptIn(ExperimentalTime::class) constructor(
+    val title: String = "",
     val currentSongId: Int? = null,
+    val status: SessionStatus = SessionStatus.Empty,
     val startTime: Instant
 )
