20542a02e64ede445ea8894dd095ac5b77878f1f
Akhil Jose <akhiljose@gmail.com>
2026-01-18 10:55:45 +0530

    change from startInstant to elapsedTime

--- app/src/main/java/com/akzuza/moosik/entities/Session.kt
+++ app/src/main/java/com/akzuza/moosik/entities/Session.kt
@@ -1,7 +1,7 @@
 package com.akzuza.moosik.entities
 
+import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
 
 enum class SessionStatus {
     Empty, Play, Pause, Completed
@@ -11,5 +11,8 @@ data class Session @OptIn(ExperimentalTime::class) constructor(
     val title: String = "",
     val currentSongId: Int? = null,
     val status: SessionStatus = SessionStatus.Empty,
-    val startTime: Instant
-)
+    val elapsed: Duration = Duration.ZERO,
+    val maxDuration: Duration = Duration.INFINITE
+) {
+    fun getProgress() = (elapsed / maxDuration).toFloat()
+}
