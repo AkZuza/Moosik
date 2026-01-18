acfeb93ac0ce3e054ea709f983de3cf26a44369e
Akhil Jose <akhiljose@gmail.com>
2026-01-18 11:08:29 +0530

    add startSession and removeSession functions remove updateSessionElapsedDuration

--- app/src/main/java/com/akzuza/moosik/screens/main/MainViewModel.kt
+++ app/src/main/java/com/akzuza/moosik/screens/main/MainViewModel.kt
@@ -7,12 +7,23 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
 
 class MainViewModel : ViewModel() {
-    private var _state = MutableStateFlow(MainState())
+    private var _state = MutableStateFlow(MainState(session = Session()))
     val state: StateFlow<MainState> = _state
 
     // Session control
+    fun startSession() {
+        if (state.value.session != null) {
+            _state.update {
+                it.copy(session = Session())
+            }
+        }
+
+        val session = state.value.session!!
+        updateSessionStatus(SessionStatus.Play)
+    }
     fun pauseSession() {
         if (state.value.session == null) return
         updateSessionStatus(SessionStatus.Pause)
@@ -20,8 +31,15 @@ class MainViewModel : ViewModel() {
 
     fun resumeSession() {
         if (state.value.session == null) return
+        if (state.value.session?.status == SessionStatus.Empty) return
         updateSessionStatus(SessionStatus.Play)
+    }
 
+    fun removeSession() {
+        if (state.value.session != null) {
+            updateSessionStatus(SessionStatus.Empty)
+            _state.update { it.copy(session = null) }
+        }
     }
 
     fun updateSession() {
@@ -39,17 +57,6 @@ class MainViewModel : ViewModel() {
                     session = session.copy(status = newStatus)
                 )
             }
-        }
-    }
-
-    private fun updateSessionElapsedDuration(elapsed: Duration) {
-        if (state.value.session == null) return
-        val session: Session = state.value.session!!
-
-        _state.update {
-            it.copy(
-                session = session.copy(elapsed = elapsed)
-            )
         }
     }
 }
