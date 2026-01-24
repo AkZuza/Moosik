9aa961f78ed5abf5a9006176c9ecd196af8ee927
Akhil Jose <akhiljose@gmail.com>
2026-01-24 11:35:38 +0530

    add contentUri member

--- app/src/main/java/com/akzuza/moosik/entities/Music.kt
+++ app/src/main/java/com/akzuza/moosik/entities/Music.kt
@@ -2,6 +2,7 @@ package com.akzuza.moosik.entities
 
 data class Music (
     val id: Int = 0,
+    val uri: String = "",
     val title: String = "",
     val album: String = "",
     val totalDurationMs: Int = 0
