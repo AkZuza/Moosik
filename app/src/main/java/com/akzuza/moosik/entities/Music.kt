afbe3b3760073120197a24789d065a95f7f2bfad
Akhil Jose <akhiljose@gmail.com>
2026-01-24 11:16:57 +0530

    add totalDurationMs member and give default values for all members

--- app/src/main/java/com/akzuza/moosik/entities/Music.kt
+++ app/src/main/java/com/akzuza/moosik/entities/Music.kt
@@ -2,6 +2,7 @@ package com.akzuza.moosik.entities
 
 data class Music (
     val id: Int = 0,
-    val title: String,
-    val album: String,
+    val title: String = "",
+    val album: String = "",
+    val totalDurationMs: Int = 0
 )
