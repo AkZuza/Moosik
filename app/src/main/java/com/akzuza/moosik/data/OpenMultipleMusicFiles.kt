289aebbd1e8a7440b8ab150356c421f86975aba9
Akhil Jose <akhiljose@gmail.com>
2026-01-27 13:19:01 +0530

    add OpenMultipleMusicFiles subclass of OpenMultipleDocuments

---
+++ app/src/main/java/com/akzuza/moosik/data/OpenMultipleMusicFiles.kt
@@ -0,0 +1,7 @@
+package com.akzuza.moosik.data
+
+import androidx.activity.result.contract.ActivityResultContracts
+
+class OpenMultipleMusicFiles : ActivityResultContracts.OpenMultipleDocuments() {
+
+}
